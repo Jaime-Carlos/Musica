7a91e000384267d275b85998632fccfd66cf9b4d
James <jaimea111@gmail.com>
2018-05-22 01:16:22 -0500

    Actualizando vistas

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -1,22 +1,75 @@
 package Controller;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
-
+    private static double xOffset = 0;
+    private static double yOffset = 0;
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../Vista/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(final Stage stage) throws Exception {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("../Vista/mainWindow.fxml"));
+        Scene scene = new Scene(root);
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+
+            @Override
+            public void handle(KeyEvent t) {
+                if(t.getCode()==KeyCode.ESCAPE)
+                {
+                    System.out.println("click on escape");
+                 Stage sb = (Stage)stage.getScene().getWindow();//use any one object
+                 sb.close();
+                }
+            }
+        });
+        
+        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = stage.getX();
+                yOffset = stage.getY();
+            }
+        });
+        
+        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX());
+                stage.setY(event.getScreenY());
+            }
+        });
+        
+        
+        //scene.getStylesheets().add("/styles/Styles.css");
+       // stage.getIcons().add(new Image("/img/mainIcon.png"));
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setTitle("Music App!");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
     }
 
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
+
 }
---
+++ src/main/java/Controller/MainWindowController.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author james
+ */
+public class MainWindowController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */ 
+    
+    @FXML
+    private void registrarAlbumOnAction(ActionEvent event)throws IOException {
+        Parent home_page_parent = FXMLLoader.load(getClass().getResource("/fxml/registroAlbum.fxml"));
+        Scene home_scene_page = new Scene(home_page_parent);
+        Stage app_stage = new Stage();
+        Stage main_app = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //(Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(home_scene_page);
+        app_stage.getIcons().add(new Image("img/mainIcon.png"));
+        app_stage.initOwner(main_app);
+        app_stage.setResizable(false);
+        app_stage.setTitle("Registro de Venta de Albúm");
+        app_stage.initModality(Modality.APPLICATION_MODAL);
+        app_stage.showAndWait();
+        System.out.println("You clicked registrar album!");
+    }
+    
+    @FXML
+    private void registrarCancionOnAction(ActionEvent event) throws IOException {
+         Parent home_page_parent = FXMLLoader.load(getClass().getResource("/fxml/registroCancion.fxml"));
+        Scene home_scene_page = new Scene(home_page_parent);
+        Stage app_stage = new Stage();
+        Stage main_app = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //(Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(home_scene_page);
+        app_stage.getIcons().add(new Image("img/mainIcon.png"));
+        app_stage.initOwner(main_app);
+        app_stage.setResizable(false);
+        app_stage.setTitle("Registro de Venta de Canción");
+        app_stage.initModality(Modality.APPLICATION_MODAL);
+        app_stage.showAndWait();
+    }
+    
+    @FXML
+    private void crearCierreOnAction(ActionEvent event) throws IOException{
+        System.out.println("You clicked registrar cierre!");
+        Parent home_page_parent = FXMLLoader.load(getClass().getResource("/fxml/crearCierre.fxml"));
+        Scene home_scene_page = new Scene(home_page_parent);
+        Stage app_stage = new Stage();
+        Stage main_app = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //(Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(home_scene_page);
+        app_stage.getIcons().add(new Image("img/mainIcon.png"));
+        app_stage.initOwner(main_app);
+        app_stage.setResizable(false);
+        app_stage.setTitle("Generar Fecha de Corte");
+        app_stage.initModality(Modality.APPLICATION_MODAL);
+        app_stage.showAndWait();
+    }
+    
+    @FXML
+    private void reportesOnAction(ActionEvent event) throws IOException {
+        Parent home_page_parent = FXMLLoader.load(getClass().getResource("/fxml/reportesMenu.fxml"));
+        Scene home_scene_page = new Scene(home_page_parent);
+        Stage app_stage = new Stage();
+        Stage main_app = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //(Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(home_scene_page);
+        app_stage.getIcons().add(new Image("img/mainIcon.png"));
+        app_stage.initOwner(main_app);
+        app_stage.setResizable(false);
+        app_stage.setTitle("Tipos de Reportes");
+        app_stage.initModality(Modality.APPLICATION_MODAL);
+        app_stage.showAndWait();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }   
+}
