ea9a1b5ec4a2e854e8508125badcd0da37146007
Carlos Daniel Bayona <carlosdanielbayo@gmail.com>
2018-05-22 10:46:50 -0500

    completo union front y back

--- src/main/java/Controller/RegistroCancionController.java
+++ src/main/java/Controller/RegistroCancionController.java
@@ -6,7 +6,18 @@
 package Controller;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import Administradores.AdminRegistroVentaSencillo;
+import Administradores.AdminReporteSencillo;
+import DAOS.DAOAlbum;
+import DAOS.DAOCanciones;
+import Modelo.Album;
+import Modelo.Cancion;
+import Modelo.VentasSencillo;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -23,10 +34,13 @@ public class RegistroCancionController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    
+    @FXML
+    private JFXComboBox sencillo;
+    @FXML
+    private JFXDatePicker fechaDeCompra;
     @FXML
     Spinner<Integer> spinner;
-    
+    AdminRegistroVentaSencillo adminRegistroVentaSencillo=new AdminRegistroVentaSencillo();
     private void registrarCancionOnAction(ActionEvent event){
         System.out.println("Registrando cancion");
     }
@@ -41,6 +55,17 @@ public class RegistroCancionController implements Initializable {
                 new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 5000, initialValue);
         spinner.setValueFactory(valueFactory);
         spinner.setEditable(true);
-    }    
-    
+        DAOCanciones daoCanciones = new DAOCanciones();
+        List<Cancion> canciones = daoCanciones.ListaCanciones();
+        for (int i = 0; i < canciones.size(); i++) {
+            sencillo.getItems().add(canciones.get(i).getTitulo());
+        }
+    }
+    @FXML
+    public void generarVenta() {
+        String fecha=fechaDeCompra.getValue().toString();
+        int numeroDeVentas=spinner.getValue();
+        Cancion tituloCancion=new Cancion(sencillo.getValue().toString());
+        adminRegistroVentaSencillo.registrarVentasSencillo(new VentasSencillo(numeroDeVentas,tituloCancion,fecha));
+    }
 }
